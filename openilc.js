5da6322d442548b129efa5994f326a5fe5b62046
HuiDong Wu <wuhd@cn.ibm.com>
2019-05-22 13:14:50 +0800

    edit

--- openilc.js
+++ openilc.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-await-in-loop */
 const open = require('open');
 const robot = require('robotjs');
 const inquirer = require('inquirer');
 const jsonfile = require('jsonfile');
 const chalk = require('chalk');
 const os = require('os');
+const delay = require('delay');
 
 const { preProcess, paramfile, updateParam } = require('./robotfunc');
 
@@ -36,10 +38,21 @@ async function openilc() {
   }
   log(chalk.green('打开ILC，8秒后输入密码'));
   await open('/Applications/ILC.app');
-  setTimeout(async () => {
-    robot.typeString(params.ilcpass);
-    robot.keyTap('enter');
-  }, 8000);
+  await delay(8000);
+  robot.typeString(params.ilcpass);
+  robot.keyTap('enter');
+  await delay(8000);
+  for (let i = 0; i < 20; i += 1) {
+    robot.keyTap('tab');
+    await delay(500);
+  }
+  for (let i = 0; i < 5; i += 1) {
+    robot.keyTap('8');
+    await delay(500);
+    robot.keyTap('tab');
+  }
+  await delay(500);
+  robot.keyTap('f6');
 }
 
 module.exports.openilc = openilc;
